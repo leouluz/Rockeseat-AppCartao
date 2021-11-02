9c5a57640cc7326ad89ed5d6b0763246a806c74d
Leonardo Luz <39962306+leouluz@users.noreply.github.com>
2021-11-02 19:20:59 -0300

    styled button in app.routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { MaterialIcons } from '@expo/vector-icons'
 import { useTheme } from 'styled-components';
 
 import { Register } from '../screens/Register';
@@ -20,21 +21,49 @@ export default function AppRoutes() {
         tabBarLabelPosition: 'beside-icon',
         tabBarStyle:{
           height: 88,
-          paddingVertical: Platform.OS === 'ios' ? 20 : 0 
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0, 
         }
       }}
     >
       <Screen 
         name="Listagem"
         component={Dashboard}
+        options={{
+          tabBarIcon: (({size, color}) => 
+          <MaterialIcons 
+            name="format-list-bulleted"
+            size={size}
+            color={color}
+          />
+          )
+        }}
       />
       <Screen 
         name="Register"
         component={Register}
+        options={{
+          tabBarIcon: (({size, color}) => 
+          <MaterialIcons 
+            name="attach-money"
+            size={size}
+            color={color}
+          />
+          )
+        }}
+        
       />
       <Screen 
         name="Resumo"
         component={Register}
+        options={{
+          tabBarIcon: (({size, color}) => 
+          <MaterialIcons 
+            name="pie-chart"
+            size={size}
+            color={color}
+          />
+          )
+        }}
       />
     </Navigator>
   );
